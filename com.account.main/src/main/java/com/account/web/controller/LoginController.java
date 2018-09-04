cde7529440f5f48fbc260caa4700822dbef50561
lunsh <martin.shi@nidone.com.cn>
2018-09-04 10:23:07 +0800

    添加修改密码与登录

--- com.account.main/src/main/java/com/account/web/controller/LoginController.java
+++ com.account.main/src/main/java/com/account/web/controller/LoginController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
