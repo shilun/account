b7c206f244a1c5ee66271b43159a74fb99ad2c95
lz <782168307@qq.com>
2018-11-07 18:19:11 +0800

    充值记录

--- com.account.web/src/main/java/com/account/web/controller/user/AccountDetailController.java
+++ com.account.web/src/main/java/com/account/web/controller/user/AccountDetailController.java
@@ -1,18 +1,25 @@
 package com.account.web.controller.user;
 
 import com.account.domain.AccountDetail;
+import com.account.domain.module.ChargeTypeEnum;
 import com.account.rpc.dto.AccountDetailDto;
 import com.account.service.AccountDetailtService;
 import com.account.web.AbstractClientController;
 import com.common.util.BeanCoper;
+import com.common.util.GlosseryEnumUtils;
+import com.passport.rpc.dto.UserDTO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -27,8 +34,18 @@ public class AccountDetailController extends AbstractClientController {
     @ApiOperation(value = "查询记录",notes = "bizToken: 1,充值;2,提款;3,赠送 {bizToken:1,pageInfo{page:0,size:30}}")
     public Map<String, Object> list(@RequestBody AccountDetailDto detailDto) {
         return buildMessage(() -> {
+            UserDTO userDTO = getUserDto();
             AccountDetail accountDetail = BeanCoper.copyProperties(AccountDetail.class,detailDto);
-            return accountDetailtService.queryByPage(accountDetail,detailDto.getPageinfo().getPage());
+            accountDetail.setPin(userDTO.getPin());
+            Page<AccountDetail> accountDetails = accountDetailtService.queryByPage(accountDetail, detailDto.getPageinfo().getPage());
+            List<AccountDetailDto> list = new ArrayList<>();
+            for(AccountDetail detail : accountDetails.getContent()){
+                AccountDetailDto dto =  BeanCoper.copyProperties(AccountDetailDto.class,detail);
+                dto.setChargeTypeName(GlosseryEnumUtils.getItem(ChargeTypeEnum.class,dto.getChargeType()).getName());
+                list.add(dto);
+            }
+            Page<AccountDetailDto> page =  new PageImpl<>(list,detailDto.getPageinfo().getPage(),accountDetails.getTotalElements());
+            return page;
         });
     }
 }
