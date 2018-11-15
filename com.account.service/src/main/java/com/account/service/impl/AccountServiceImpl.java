bc9537d3755e8036590a2b397b73efc7f6bc0bfb
lz <782168307@qq.com>
2018-11-15 18:34:12 +0800

    userCode

--- com.account.service/src/main/java/com/account/service/impl/AccountServiceImpl.java
+++ com.account.service/src/main/java/com/account/service/impl/AccountServiceImpl.java
@@ -163,12 +163,12 @@ public class AccountServiceImpl extends DefaultBaseService<Account> implements A
                 account.setTest(dto.getTest());
                 account.setProxyId(dto.getProxyId());
                 account.setPin(dto.getPin());
-                account.setUserCode(dto.getUserCode());
+                account.setUserCode(byPin.getData().getId());
             }
             AccountDetail detail = new AccountDetail();
             detail.setIsRobot(isRobot);
             detail.setPin(dto.getPin());
-            detail.setUserCode(dto.getUserCode());
+            detail.setUserCode(byPin.getData().getId());
             detail.setTest(dto.getTest());
             detail.setProxyId(dto.getProxyId());
             detail.setTokenType(dto.getTokenType());
@@ -213,7 +213,7 @@ public class AccountServiceImpl extends DefaultBaseService<Account> implements A
                 upEntity.setId(account.getId());
                 upEntity.setAmount(account.getAmount());
                 upEntity.setFreeze(account.getFreeze());
-                upEntity.setUserCode(account.getUserCode());
+                upEntity.setUserCode(byPin.getData().getId());
                 up(upEntity);
             }
             detail.setStatus(DetailStatusEnum.Normal.getValue());
